a5b174589ae190c3ff671942b46e68d61580bb13
ndboscher <nboscher4@gmail.com>
2025-09-09 11:37:56 -0400

    feat: Update contact section to use email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -220,12 +220,12 @@ export default function Page() {
                 Get in Touch
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Want to chat? Just shoot me a DM{" "}
+                Want to chat? Just shoot me an{" "}
                 <Link
-                  href={DATA.contact.social.X.url}
+                  href={`mailto:${DATA.contact.email}`}
                   className="text-blue-500 hover:underline"
                 >
-                  with a direct question on Twitter
+                  email
                 </Link>{" "}
                 and I&apos;ll respond whenever I can. I will ignore all
                 soliciting.
